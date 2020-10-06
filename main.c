1b520144fbe60ea75cc68f6cd4c613220e9515f9
SubbotinSAS <67541139+SubbotinSAS@users.noreply.github.com>
2020-10-06 21:25:04 +0300

    DZ1

--- main.c
+++ main.c
@@ -1,80 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define QMAX 100
 
-//void pri(int n){
-    //for (int y=0;y<n+1;y++){
-        //printf("| %d |",y);
-   // }
-//}
+struct queue {
+  int qu[QMAX];
+  int rear, frnt;
+};
 
-void init(int arr[][30],int n){
-    FILE *fp1;
-    fp1=fopen("DZ2.dot", "w");
-    int kk=0;
-    fprintf(fp1,"graph DZ2 {\n");
-    for (int ii=0;ii<n;ii++) {
-        for (int jj=0;jj<n;jj++) {
-            for (int kj=0;kj<arr[ii][jj];kj++) {
-                fprintf(fp1,"\t%d -- %d;\n",ii,jj);
-            if (ii!=jj){
-                arr[jj][ii]--;
-            }
-        }
-            if (arr[ii][jj]!=0) {
-                kk++;
-            }
-        }
-            if (kk==0) {
-            fprintf(fp1,"\t%d;\n",ii);
-        }
-        kk=0;
-    }
-    fprintf(fp1,"}");
-    fclose(fp1);
-
-    system("dot -Tpng DZ2.dot -oDZ2.png");
+void init(struct queue *q) {
+  q->frnt = 1;
+  q->rear = 0;
 }
 
+void insert(struct queue *q, int x) {
+  if(q->rear < QMAX-1) {
+    q->rear++;
+    q->qu[q->rear]=x;
+  }
+  else
+    printf("Queue cannot be over 100\n");
+}
 
+int isempty(struct queue *q) {
+  if(q->rear < q->frnt)
+        return 1;
+  else
+        return 0;
+}
+
+void print(struct queue *q) {
+  int h;
+  if(isempty(q)==1) {
+    printf("Queue is empty\n");
+    return;
+  }
+  for(h = q->frnt; h<= q->rear; h++)
+    printf("%d  ",q->qu[h]);
+}
+
+int removex(struct queue *q) {
+  int x, h;
+  if(isempty(q)==1) {
+    printf("Queue is empty\n");
+    return 0;
+  }
+  x = q->qu[q->frnt];
+  for(h = q->frnt; h < q->rear; h++) {
+    q->qu[h] = q->qu[h+1];
+  }
+  q->rear--;
+  return x;
+}
+
+int ent(void){
+    int n;
+    printf("\nIf you want to add values, type 1\nIf you want to delete value, type 2\nIf you want to see all values, type 3\n");
+    scanf("%d",&n);
+    return n;
+}
+
+void select(int n,struct queue *q){
+    if(n==1){
+        system("cls");
+        int x;
+        printf("Add value: ");
+        scanf("%d",&x);
+        insert(q,x);
+        system("cls");
+        select(ent(),q);
+    }
+    else if(n==2){
+        system("cls");
+        removex(q);
+        select(ent(),q);
+    }
+    else if(n==3){
+        system("cls");
+        print(q);
+        getchar();
+        select(ent(),q);
+    }
+    else{
+        system("cls");
+        printf("Wrong value\n");
+        getchar();
+        select(ent(),q);
+    }
+}
 
 int main(void){
-    int arr[30][30];
-    int n;
-    printf("Enter number of vertices: ");
-    scanf("%d",&n);
-    getchar();
-    for (int i=0;i<n;i++){
-        for (int k=0;k<n;k++){
-            arr[i][k] = 0;
-        }
-    }
-    int wer;
-    int ver;
-    //int rt=0;
-    for (int t=0;t<n;t++){
-        printf("\nHow many of vertices the vertex %d connects to: ",t);
-        scanf("%d",&wer);
-    //if (wer>=2)
-        //rt++;
-        printf("\nWhich vertices the vertex %d connects to: ",t);
-    for (int te=0;te<wer;te++){
-        scanf("%d",&ver);
-        arr[t][ver]=1;
-    }
-    }
-    //pri(n);
-    printf("\n");
-    for (int ik=0;ik<n;ik++){
-        //printf("| %d |",ik);
-        for (int ki=0;ki<n;ki++){
-            printf("| %d |",arr[ik][ki]);
-        }
-        printf("\n");
-    }
-    init(arr,n);
-    //if (rt<n-2)
-        //printf("Unconnected graph");
-    //else
-        //printf("Connected graph");
+    struct queue q;
+    init(&q);
+    select(ent(),&q);
     return 0;
 }
